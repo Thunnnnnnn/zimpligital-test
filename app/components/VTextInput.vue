311f3f6cee15a1fe3f78d38fb5f013dc9512c948
Thunnn <thunwi@hotmail.com>
2026-01-18 20:04:26 +0700

    Set up project

---
+++ app/components/VTextInput.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+const props = defineProps({
+    label: {
+        type: String,
+        default: '',
+    },
+    placeholder: {
+        type: String,
+        default: '',
+    },
+    modelValue: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div>
+        <label for="inputText" class="form-label">{{ label }}</label>
+        <input type="text" class="form-control" :placeholder="placeholder" :value="modelValue"
+            @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" />
+    </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+const test = ref('')
+
+</script>
+
+<template>
+    <div class="row container">
+        <VTextInput v-model="test" label="test" placeholder="test" />
+    </div>
+</template>
